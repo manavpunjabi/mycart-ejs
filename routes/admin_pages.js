093bfa78163752ca1455a3eef1d4411de1bf9d4c
Manav Punjabi <manavpunjabi26@gmail.com>
2020-01-14 22:32:02 +0530

    Added edit page

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
+// get page model
+const Page = require("../models/page");
+
 // get pages index
 
 // default route is /admin/pages
 router.get("/", (req, res) => {
-  res.send("admin area");
+  Page.find({})
+    .sort({ sorting: 1 })
+    .exec((err, pages) => {
+      res.render("admin/pages", {
+        pages: pages
+      });
+    });
 });
 
 // get add page
@@ -44,7 +53,124 @@ router.post("/add-page", (req, res) => {
       content: content
     });
   } else {
-    console.log("Success");
+    Page.findOne({ slug: slug }, (err, page) => {
+      if (page) {
+        req.flash("danger", "Page slug exists, choose another");
+        res.render("admin/add_page", {
+          title: title,
+          slug: slug,
+          content: content
+        });
+      } else {
+        let page = new Page({
+          title: title,
+          slug: slug,
+          content: content,
+          sorting: 100
+        });
+        page.save(err => {
+          if (err) {
+            return console.log(err);
+          }
+          req.flash("success", "Page added");
+          res.redirect("/admin/pages");
+        });
+      }
+    });
+  }
+});
+
+//post reorder pages
+router.post("/reorder-pages", (req, res) => {
+  // console.log(req.body);
+  let ids = req.body["id[]"];
+  let count = 0;
+
+  for (let i = 0; i < ids.length; i++) {
+    let id = ids[i];
+    count++;
+
+    (function(count) {
+      Page.findById(id, (err, page) => {
+        page.sorting = count;
+        page.save(err => {
+          if (err) {
+            return console.log(err);
+          }
+        });
+      });
+    })(count);
+  }
+});
+
+// get edit page
+router.get("/edit-page/:slug", (req, res) => {
+  Page.findOne({ slug: req.params.slug }, (err, page) => {
+    if (err) {
+      return console.log(err);
+    }
+    res.render("admin/edit_page", {
+      title: page.title,
+      slug: page.slug,
+      content: page.content,
+      id: page._id
+    });
+  });
+});
+
+// post edit page
+router.post("/edit-page/:slug", (req, res) => {
+  req.checkBody("title", "Title must have value").notEmpty();
+  req.checkBody("content", "Content must have value").notEmpty();
+
+  let title = req.body.title;
+  let slug = req.body.slug.replace(/\s+/g, "-").toLowerCase();
+  // making title the slug if slug is empty as its not set to REQUIRED
+  if (slug == "") {
+    slug = title.replace(/\s+/g, "-").toLowerCase();
+  }
+  let content = req.body.content;
+  let id = req.body.id;
+
+  let errors = req.validationErrors();
+  if (errors) {
+    console.log("errors in validation");
+    res.render("admin/edit_page", {
+      errors: errors,
+      title: title,
+      slug: slug,
+      content: content,
+      id: id
+    });
+  } else {
+    Page.findOne({ slug: slug, _id: { $ne: id } }, (err, page) => {
+      if (page) {
+        req.flash("danger", "Page slug exists, choose another");
+        res.render("admin/edit_page", {
+          title: title,
+          slug: slug,
+          content: content,
+          id: id
+        });
+      } else {
+        Page.findById(id, (err, page) => {
+          if (err) {
+            return console.log(err);
+          }
+          page.title = title;
+          page.slug = slug;
+          page.content = content;
+
+          page.save(err => {
+            if (err) {
+              return console.log(err);
+            }
+            req.flash("success", "Page added");
+            res.redirect("/admin/edit-page/" + page.slug);
+          });
+        });
+      }
+    });
   }
 });
 
