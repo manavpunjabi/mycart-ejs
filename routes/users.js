ed31154b684a86892935c32388f2f8a7be13bfc9
Manav Punjabi <manavpunjabi26@gmail.com>
2020-01-22 15:50:09 +0530

    added register page

--- routes/users.js
+++ routes/users.js
@@ -11,22 +11,71 @@ router.get("/register", (req, res) => {
   });
 });
 
-//get a page
+// post register
+router.post("/register", (req, res) => {
+  let name = req.body.name;
+  let email = req.body.email;
+  let username = req.body.username;
+  let password = req.body.password;
+  let password2 = req.body.password2;
 
-router.get("/:slug", (req, res) => {
-  let slug = req.params.slug;
-  Page.findOne({ slug: slug }, (err, page) => {
-    if (err) {
-      console.log(err);
-    }
-    if (!page) {
-      res.redirect("/");
-    } else {
-      res.render("index", {
-        title: page.title,
-        content: page.content
-      });
-    }
+  req.checkBody("name", "Name is mandatory").notEmpty();
+  req.checkBody("email", "Email is mandatory").isEmail();
+  req.checkBody("username", "Username is mandatory").notEmpty();
+  req.checkBody("password", "Password is mandatory").notEmpty();
+  req.checkBody("password2", "Passwords do not match").equals(password);
+
+  let errors = req.validationErrors();
+  if (errors) {
+    res.render("register", {
+      errors: errors,
+      title: "mycart - Sign Up"
+    });
+  } else {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) {
+        console.log(err);
+      }
+      if (user) {
+        req.flash("danger", "Username exists");
+        res.redirect("/users/register");
+      } else {
+        let user = new User({
+          name: name,
+          email: email,
+          username: username,
+          password: password,
+          // put 1 to make admins
+          admin: 0
+        });
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err) {
+              console.log(err);
+            }
+            user.password = hash;
+            user.save(err => {
+              if (err) {
+                console.log(err);
+              } else {
+                req.flash("success", "Sign up successful");
+                res.redirect("/users/login");
+              }
+            });
+          });
+        });
+      }
+    });
+  }
+});
+
+// get login
+router.get("/login", (req, res) => {
+  if (res.locals.user) {
+    res.redirect("/");
+  }
+  res.render("login", {
+    title: "mycart - Login"
   });
 });
 
